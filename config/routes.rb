48a9cba447e166fcdec35489a3546f14671bb279
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-01-31 01:33:56 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :students, only: :index
 
   get "students/:id", to: "students#show", as: "student"
-  get "students/:id/activate", to: "students#activate", as: "activate_student" to: redirect("/students/#{params[@student.id]}")
+  get "students/:id/activate", to: "students#activate", as: "activate_student", to: redirect("/students/#{params[@student.id]}")
 end
