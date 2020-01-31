1562dd9b6bc2e9220657b51141c505366fdf99ba
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-01-31 01:02:13 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,23 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @students = Student.find(params[:id])
+  end
+
+  def activate
+    @student ||= Student.find(params[:id])
+    if @student.active == true
+      @student.active = false
+      @student.save
+    elsif @student.active == false
+      @student.active = true
+    end
+    @student.active
   end
 
   private
